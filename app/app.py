d6104f9869700fe371dc88864ee30ebef338942c
orestis <ob00042@gmail.com>
2019-08-02 00:04:41 +0100

    Almost done.

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, jsonify, redirect, flash
 import os
 from werkzeug.utils import secure_filename
-import csv
+# import csv
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 app = Flask(__name__)
-
+app.config['filename_train'] = ''
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
@@ -19,21 +21,111 @@ def upload():
         flash('No file part')
         return redirect(request.url)
     csv = request.files.get('csv')
-    filename = secure_filename(csv.filename)
-    f = open('app/uploads/' + filename, 'wb+')
+    filename_train = secure_filename(csv.filename)
+    app.config['filename_train'] = filename_train
+    f = open('app/uploads/' + filename_train, 'wb+')
     f.close()
-    csv.save(os.path.join('app/uploads', filename))
+    csv.save(os.path.join('app/uploads', filename_train))
     return jsonify({'status': 'Uploaded'})
 
 
 @app.route("/analyse", methods=["POST"])
 def analyse():
-    csv = pd.read_csv('app/uploads/bank-full.csv')
-    print(csv.columns)
-    print(csv.values[1])
-    return jsonify({'status': 'Analysed'})
+    df = pd.read_csv('app/uploads/' + app.config['filename_train'], delimiter=';', skipinitialspace=True)
+
+    arr, y = process_file(df)
+
+    train_features, test_features, train_labels, test_labels = train_test_split(arr, y, test_size=0.2, random_state=42)
+
+    rf.fit(train_features, train_labels)
+
+    predictions = rf.predict(test_features)
+    c = 0
+    for p, ry in zip(predictions, test_labels):
+        if p == ry:
+            c += 1
+    acc = c/len(test_labels)
+
+    return jsonify({'status': 'Analysed', 'accuracy': acc})
+
+
+@app.route('/test', methods=['POST'])
+def test():
+    if 'test' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    test = request.files.get('test')
+    filename = secure_filename(test.filename)
+    f = open('app/uploads/' + filename, 'wb+')
+    f.close()
+    test.save(os.path.join('app/uploads', filename))
+
+    df = pd.read_csv(os.path.join('app/uploads', filename), delimiter=';', skipinitialspace=True)
+
+    arr, y = process_file(df)
+
+    predictions = rf.predict(arr)
+    if y is not None:
+        c = 0
+        for p, ry in zip(predictions, y):
+            if p == ry:
+                c += 1
+        acc = c / len(y)
+    else:
+        acc = None
+
+    preds = []
+    for p in predictions:
+        if p == 0:
+            preds.append('yes')
+        else:
+            preds.append('no')
+
+    return jsonify({"status": "tested", "accuracy": acc, "predictions": preds})
+
+
+def process_file(df):
+    # df['job'] = df.job.astype('category')
+    # df['marital'] = df.marital.astype('category')
+    # df['education'] = df.education.astype('category')
+    # df['default'] = df.default.astype('category')
+    # df['housing'] = df.housing.astype('category')
+    # df['loan'] = df.loan.astype('category')
+    # df['contact'] = df.contact.astype('category')
+    # df['duration'] = df.duration.astype('category')
+    # df['campaign'] = df.campaign.astype('category')
+    # df['pdays'] = df.pdays.astype('category')
+    # df['previous'] = df.previous.astype('category')
+    # df['poutcome'] = df.poutcome.astype('category')
+
+    df['job'] = pd.Categorical(df.job).codes
+    df['marital'] = pd.Categorical(df.marital).codes
+    df['education'] = pd.Categorical(df.education).codes
+    df['default'] = pd.Categorical(df.default).codes
+    df['housing'] = pd.Categorical(df.housing).codes
+    df['loan'] = pd.Categorical(df.loan).codes
+    df['contact'] = pd.Categorical(df.contact).codes
+    df['month'] = pd.Categorical(df.month).codes
+    df['duration'] = pd.Categorical(df.duration).codes
+    df['campaign'] = pd.Categorical(df.campaign).codes
+    df['pdays'] = pd.Categorical(df.pdays).codes
+    df['previous'] = pd.Categorical(df.previous).codes
+    df['poutcome'] = pd.Categorical(df.poutcome).codes
+    if 'y' in df:
+        df['y'] = pd.Categorical(df.y).codes
+
+    arr = df.to_numpy()
+
+    if 'y' in df:
+        y = arr[:, -1]
+        arr = arr[:, :-1]
+    else:
+        y = None
+
+    return arr, y
 
 
 # run the server
 if __name__ == '__main__':
+    rf = RandomForestClassifier(n_estimators=1000, random_state=42)
     app.run(debug=True)
--- app/static/main.js
+++ app/static/main.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
             contentType: false,
             processData: false,
             success: function(su) {
-                console.log(su);
                 fileUploaded();
             },
             error: function(err) {
@@ -25,14 +24,41 @@ $(document).ready(function() {
     });
 
     $('#analyse').on('click', function() {
+
+        $('#analyse').html('<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>' + '  Loading...');
+
         $.ajax({
             type: 'POST',
             url: '/analyse',
-            success: function(s) {
-                console.log(s);
+            success: function(succ) {
+                $('#analyse').html('Analyse');
+                afterAnalysis(succ);
             },
             error: function(e) {
+                $('#analyse').html('Analyse');
                 console.log(e)
+            }
+        })
+    });
+
+    $('#upload-more-test').on('submit', function() {
+        $('#test-btn').html('<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>' + '  Loading...');
+        let fd = new FormData($('#upload-more-test')[0]);
+
+        $.ajax({
+            type: 'POST',
+            url: '/test',
+            data: fd,
+            cache: false,
+            dataType : 'json',
+            contentType: false,
+            processData: false,
+            success: function(succ) {
+                afterTest(succ);
+                $('#test-btn').html('test');
+            },
+            error: function(err) {
+                $('#test-btn').html('test');
             }
         })
     });
@@ -41,6 +67,32 @@ $(document).ready(function() {
         $('.upload-area').hide();
         $('#csv-uploaded').show();
         $('.analyse-area').show();
+    }
+
+    function afterAnalysis(succ) {
+        $('.after-area').show();
+        $('.upload-area').hide();
+        $('.analyse-area').hide();
+        $('#csv-analysed').show();
+        if (succ && succ.accuracy) {
+            $('#test-accuracy').html((succ.accuracy*100).toFixed(2));
+        }
+    }
+
+    function afterTest(results) {
+        $('#upload-more-test input')[0].value = '';
+        $('.test-results-area').show();
+        if (results && results.accuracy) {
+            $('.test-results-area').append('<h2>Test accuracy was: ' + (results.accuracy*100).toFixed(2) + '%</h2>');
+        }
+        if (results && results.predictions && results.predictions.length > 0) {
+            let ls = '';
+            for (let i = 0; i < results.predictions.length; i++) {
+                let lab = results.predictions[i] ? 'yes' : 'no';
+                ls += '<li class="list-group-item">' + lab + '</li>'
+            }
+            $('.test-results-area').append('<h3>The test results were:</h3><ul>' + ls + '</ul>');
+        }
     }
 
 });
