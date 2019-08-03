ac7c4852860b89778210c8f3c05539cbd6575ed6
orestis <ob00042@gmail.com>
2019-08-03 13:26:40 +0100

    Uploads data directly.

--- app/app.py
+++ app/app.py
@@ -4,11 +4,17 @@ from werkzeug.utils import secure_filename
 # import csv
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+import random
+from sklearn import svm
 
 app = Flask(__name__)
 app.config['filename_train'] = ''
+app.config['model'] = None
+app.config['categories'] = None
+app.config['columns'] = None
+
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
@@ -31,22 +37,52 @@ def upload():
 
 @app.route("/analyse", methods=["POST"])
 def analyse():
+    model = request.form.get('model')
+    if model == 'rf':
+        app.config['model'] = rf
+    elif model == 'svm':
+        app.config['model'] = svm
+    else:
+        app.config['model'] = rf
+
     df = pd.read_csv('app/uploads/' + app.config['filename_train'], delimiter=';', skipinitialspace=True)
 
     arr, y = process_file(df)
 
     train_features, test_features, train_labels, test_labels = train_test_split(arr, y, test_size=0.2, random_state=42)
 
-    rf.fit(train_features, train_labels)
+    # Cross Validation Params
 
-    predictions = rf.predict(test_features)
-    c = 0
-    for p, ry in zip(predictions, test_labels):
-        if p == ry:
-            c += 1
-    acc = c/len(test_labels)
+    # param_grid = {
+    #     'max_depth': [2, 10, 40, 80, None],
+    #     'max_features': [2, 3],
+    #     'min_samples_leaf': [3, 4, 5],
+    #     'min_samples_split': [8, 10, 12],
+    #     'n_estimators': [25, 100, 200, 300, 1000]
+    # }
+    #
+    # rf = RandomForestClassifier()
+    # # Instantiate the grid search model
+    # grid_search = GridSearchCV(estimator=rf, param_grid=param_grid,
+    #                            cv=3, n_jobs=-1, verbose=0)
+    #
+    # grid_search.fit(train_features, train_labels)
+    #
+    # print(grid_search.best_params_)
+    # best_grid = grid_search.best_estimator_
+    # grid_accuracy, grid_preds = test_preds(best_grid, test_features, test_labels)
+    #
+    # print(grid_accuracy)
+    # print(grid_preds)
+    #
+    # app.config['rf_model'] = best_grid
+    # acc = grid_accuracy
 
-    return jsonify({'status': 'Analysed', 'accuracy': acc})
+    acc = train(app.config['model'], train_features, train_labels, test_features, test_labels)
+
+    options = create_options()
+
+    return jsonify({'status': 'Analysed', 'accuracy': acc, 'options': options})
 
 
 @app.route('/test', methods=['POST'])
@@ -64,6 +100,94 @@ def test():
 
     arr, y = process_file(df)
 
+    acc, preds = test_preds(app.config['model'], arr, y)
+
+    return jsonify({"status": "tested", "accuracy": acc, "predictions": preds})
+
+
+@app.route("/direct", methods=['POST', 'OPTIONS'])
+def direct():
+    data = request.json['data']
+    df = pd.DataFrame(data, index=[0])
+    arr, y = process_file(df)
+    acc, preds = test_preds(app.config['model'], arr, y)
+
+    return jsonify({"status": "tested", "accuracy": acc, "predictions": preds})
+
+
+def process_file(df):
+
+    if app.config['columns'] is None:
+        app.config['columns'] = df.columns
+
+    if app.config['categories'] is None:
+        app.config['categories'] = {}
+        app.config['categories']['job'] = dict(enumerate(pd.Categorical(df.job).categories))
+        app.config['categories']['marital'] = dict(enumerate(pd.Categorical(df.marital).categories))
+        app.config['categories']['education'] = dict(enumerate(pd.Categorical(df.education).categories))
+        app.config['categories']['default'] = dict(enumerate(pd.Categorical(df.default).categories))
+        app.config['categories']['housing'] = dict(enumerate(pd.Categorical(df.housing).categories))
+        app.config['categories']['loan'] = dict(enumerate(pd.Categorical(df.loan).categories))
+        app.config['categories']['contact'] = dict(enumerate(pd.Categorical(df.contact).categories))
+        app.config['categories']['month'] = dict(enumerate(pd.Categorical(df.month).categories))
+        app.config['categories']['duration'] = dict(enumerate(pd.Categorical(df.duration).categories))
+        app.config['categories']['campaign'] = dict(enumerate(pd.Categorical(df.campaign).categories))
+        app.config['categories']['pdays'] = dict(enumerate(pd.Categorical(df.pdays).categories))
+        app.config['categories']['previous'] = dict(enumerate(pd.Categorical(df.previous).categories))
+        app.config['categories']['poutcome'] = dict(enumerate(pd.Categorical(df.poutcome).categories))
+        if 'y' in df:
+            app.config['categories']['y'] = dict(enumerate(pd.Categorical(df.y).categories))
+        for c in df.columns:
+            if c not in app.config['categories']:
+                app.config['categories'][c] = None
+
+    if 'y' in df:
+        df = df[app.config['columns']]
+    else:
+        cols = list(app.config['columns'])
+        cols.remove('y')
+        df = df[cols]
+
+    df['job'] = pd.Categorical(df.job, categories=app.config['categories']['job'].values()).codes
+    df['marital'] = pd.Categorical(df.marital, categories=app.config['categories']['marital'].values()).codes
+    df['education'] = pd.Categorical(df.education, categories=app.config['categories']['education'].values()).codes
+    df['default'] = pd.Categorical(df.default, categories=app.config['categories']['default'].values()).codes
+    df['housing'] = pd.Categorical(df.housing, categories=app.config['categories']['housing'].values()).codes
+    df['loan'] = pd.Categorical(df.loan, categories=app.config['categories']['loan'].values()).codes
+    df['contact'] = pd.Categorical(df.contact, categories=app.config['categories']['contact'].values()).codes
+    df['month'] = pd.Categorical(df.month, categories=app.config['categories']['month'].values()).codes
+    df['duration'] = pd.Categorical(df.duration, categories=app.config['categories']['duration'].values()).codes
+    df['campaign'] = pd.Categorical(df.campaign, categories=app.config['categories']['campaign'].values()).codes
+    df['pdays'] = pd.Categorical(df.pdays, categories=app.config['categories']['pdays'].values()).codes
+    df['previous'] = pd.Categorical(df.previous, categories=app.config['categories']['previous'].values()).codes
+    df['poutcome'] = pd.Categorical(df.poutcome, categories=app.config['categories']['poutcome'].values()).codes
+    if 'y' in df:
+        df['y'] = pd.Categorical(df.y, categories=app.config['categories']['y'].values()).codes
+
+    arr = df.to_numpy()
+
+    if 'y' in df:
+        y = arr[:, -1]
+        arr = arr[:, :-1]
+    else:
+        y = None
+
+    return arr, y
+
+
+def train(rf, train_features, train_labels, test_features, test_labels):
+    rf.fit(train_features, train_labels)
+
+    predictions = rf.predict(test_features)
+    c = 0
+    for p, ry in zip(predictions, test_labels):
+        if p == ry:
+            c += 1
+    acc = c / len(test_labels)
+    return acc
+
+
+def test_preds(rf, arr, y=None):
     predictions = rf.predict(arr)
     if y is not None:
         c = 0
@@ -77,55 +201,25 @@ def test():
     preds = []
     for p in predictions:
         if p == 0:
-            preds.append('yes')
-        else:
             preds.append('no')
+        else:
+            preds.append('yes')
+    return acc, preds
 
-    return jsonify({"status": "tested", "accuracy": acc, "predictions": preds})
 
-
-def process_file(df):
-    # df['job'] = df.job.astype('category')
-    # df['marital'] = df.marital.astype('category')
-    # df['education'] = df.education.astype('category')
-    # df['default'] = df.default.astype('category')
-    # df['housing'] = df.housing.astype('category')
-    # df['loan'] = df.loan.astype('category')
-    # df['contact'] = df.contact.astype('category')
-    # df['duration'] = df.duration.astype('category')
-    # df['campaign'] = df.campaign.astype('category')
-    # df['pdays'] = df.pdays.astype('category')
-    # df['previous'] = df.previous.astype('category')
-    # df['poutcome'] = df.poutcome.astype('category')
-
-    df['job'] = pd.Categorical(df.job).codes
-    df['marital'] = pd.Categorical(df.marital).codes
-    df['education'] = pd.Categorical(df.education).codes
-    df['default'] = pd.Categorical(df.default).codes
-    df['housing'] = pd.Categorical(df.housing).codes
-    df['loan'] = pd.Categorical(df.loan).codes
-    df['contact'] = pd.Categorical(df.contact).codes
-    df['month'] = pd.Categorical(df.month).codes
-    df['duration'] = pd.Categorical(df.duration).codes
-    df['campaign'] = pd.Categorical(df.campaign).codes
-    df['pdays'] = pd.Categorical(df.pdays).codes
-    df['previous'] = pd.Categorical(df.previous).codes
-    df['poutcome'] = pd.Categorical(df.poutcome).codes
-    if 'y' in df:
-        df['y'] = pd.Categorical(df.y).codes
-
-    arr = df.to_numpy()
-
-    if 'y' in df:
-        y = arr[:, -1]
-        arr = arr[:, :-1]
-    else:
-        y = None
-
-    return arr, y
+def create_options():
+    opts = {}
+    for k, it in app.config['categories'].items():
+        if not it:
+            opts[k] = None
+        else:
+            opts[k] = list(it.values())
+    return opts
 
 
 # run the server
 if __name__ == '__main__':
-    rf = RandomForestClassifier(n_estimators=1000, random_state=42)
+    best = {'max_depth': None, 'max_features': 3, 'min_samples_leaf': 3, 'min_samples_split': 8, 'n_estimators': 300}
+    rf = RandomForestClassifier(**best)
+    svm = svm.SVC(gamma='scale')
     app.run(debug=True)
--- app/static/main.js
+++ app/static/main.js
@@ -25,11 +25,14 @@ $(document).ready(function() {
 
     $('#analyse').on('click', function() {
 
+        let model = $('.model-choice:checked').val();
+
         $('#analyse').html('<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>' + '  Loading...');
 
         $.ajax({
             type: 'POST',
             url: '/analyse',
+            data: {'model': model},
             success: function(succ) {
                 $('#analyse').html('Analyse');
                 afterAnalysis(succ);
@@ -55,12 +58,41 @@ $(document).ready(function() {
             processData: false,
             success: function(succ) {
                 afterTest(succ);
-                $('#test-btn').html('test');
+                $('#test-btn').html('Test');
             },
             error: function(err) {
-                $('#test-btn').html('test');
+                $('#test-btn').html('Test');
             }
         })
+    });
+
+    $('#enter-data-form').on('submit', function() {
+        $('#test-directly').html('<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>' + '  Loading...');
+        let data = {'data': {}};
+
+        $('#enter-data-form select, #enter-data-form input').each((i, el) => {
+           let n = $(el).attr('name');
+           let v = $(el).val();
+           if (['day', 'balance', 'age'].includes(n)) {
+               v = parseInt(v);
+           }
+           data['data'][n] = v;
+        });
+
+        $.ajax({
+            type: 'POST',
+            url: '/direct',
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            dataType: "json",
+            success: function(succ) {
+                afterTest(succ);
+                $('#test-directly').html('Test directly');
+            },
+            error: function(err) {
+                $('#test-directly').html('Test directly');
+            }
+        });
     });
 
     function fileUploaded() {
@@ -70,26 +102,44 @@ $(document).ready(function() {
     }
 
     function afterAnalysis(succ) {
+        if (succ && succ.options) {
+            showOptions(succ.options);
+        }
         $('.after-area').show();
         $('.upload-area').hide();
         $('.analyse-area').hide();
         $('#csv-analysed').show();
-        if (succ && succ.accuracy) {
+        if (succ && succ.accuracy && typeof succ.accuracy === 'number') {
             $('#test-accuracy').html((succ.accuracy*100).toFixed(2));
         }
+    }
+
+    function showOptions(opts) {
+        Object.keys(opts).forEach(function(key) {
+            if (opts[key]) {
+                let selectOpts = '';
+                opts[key].forEach((v) => {selectOpts += '<option value="' + v + '">' + v + '</option>'});
+                $('#enter-data-form').prepend('<div class="form-group row"><label for="enter-' + key + '" class="col-sm-2 col-form-label">' + key + '</label>' +
+                    '<div class="col-sm-10"><select class="form-control custom-select" name="' + key + '" id="enter-' + key + '">' + selectOpts + '</select></div></div>');
+            }
+            else {
+                $('#enter-data-form').prepend('<div class="form-group row"><label for="enter-' + key + '" class="col-sm-2 col-form-label">' + key + '</label>' +
+                    '<div class="col-sm-10"><input name="' + key + '" id="enter-' + key + '" class="form-control"></div></div>');
+            }
+        });
+
     }
 
     function afterTest(results) {
         $('#upload-more-test input')[0].value = '';
         $('.test-results-area').show();
-        if (results && results.accuracy) {
+        if (results && results.accuracy !== null && typeof results.accuracy === 'number') {
             $('.test-results-area').append('<h2>Test accuracy was: ' + (results.accuracy*100).toFixed(2) + '%</h2>');
         }
         if (results && results.predictions && results.predictions.length > 0) {
             let ls = '';
             for (let i = 0; i < results.predictions.length; i++) {
-                let lab = results.predictions[i] ? 'yes' : 'no';
-                ls += '<li class="list-group-item">' + lab + '</li>'
+                ls += '<li class="list-group-item">' + results.predictions[i] + '</li>'
             }
             $('.test-results-area').append('<h3>The test results were:</h3><ul>' + ls + '</ul>');
         }
